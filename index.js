096ed8367505e892e78925db3eb8f0c25b9e05c6
feelipino <fe.iacarneiro@gmail.com>
2025-04-04 13:53:27 -0300

    fix: removed unnecessary comments.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Event Organization
-const startTime = "14:00";
+const startTime = "14:00"; // always in xx:xx format
 const endTime = "17:00";
-const breakDuration = "00:30"; //minutes
+const breakDuration = "00:30";
 const slideNum = 40;
 
 // Function to convert the time to minutes
 function convertTimetoMinutes(time) {
     let timeParts = time.split(":");
     return Number(timeParts[0] * 60 + Number(timeParts[1]));
-}
+};
 
 // Time in minutes
 const startTimeinMinutes = convertTimetoMinutes(startTime);
 const endTimeinMinutes = convertTimetoMinutes(endTime);
 const eventDurationinMinutes = endTimeinMinutes - startTimeinMinutes;
 const breakDurationinMinutes = convertTimetoMinutes(breakDuration);
-const avaliableTimetoPresent = eventDurationinMinutes - breakDurationinMinutes
+const avaliableTimetoPresent = eventDurationinMinutes - breakDurationinMinutes;
 
 // Break
 const breakStart = startTimeinMinutes + (eventDurationinMinutes - breakDurationinMinutes) / 2;
-const breakEnd = breakStart + breakDurationinMinutes
+const breakEnd = breakStart + breakDurationinMinutes;
 
 // Slide per minutes
 const timePerSlide = avaliableTimetoPresent / slideNum;
@@ -37,10 +37,10 @@ for (let i = 1; i <= firstPartSlideAmount; i++) {
 function breakTimetoHours (time) {
     let hours = Math.floor(time/ 60);
     let minutes = time % 60;
-    return hours + ":" + Math.floor(minutes)
+    return hours + ":" + Math.floor(minutes);
 }
 
-console.log(`Break ${breakTimetoHours(breakStart)}h to ${breakTimetoHours(breakEnd)}h`)
+console.log(`Break ${breakTimetoHours(breakStart)}h to ${breakTimetoHours(breakEnd)}h`);
 
 //Event second time code setup
 const secondPartSlideAmount = slideNum / 2;
@@ -49,45 +49,6 @@ for (let i = 1; i <= secondPartSlideAmount; i++) {
     let slideTimeRange = breakEnd + ( timePerSlide * i );
     let hours = Math.floor(slideTimeRange / 60);
     let minutes = Math.floor(slideTimeRange % 60);
-    let slideCount = secondPartSlideAmount + i
+    let slideCount = secondPartSlideAmount + i;
     console.log(`Slide ${slideCount} by ${hours}:${minutes}h`);
-}
-
-
-/*
-function firstPartApresentation () {
-    let slideTimeRange = startTimeinMinutes + timePerSlide;
-    console.log(slideTimeRange)
-
-    function slideTimeRangetoHours (slideTimeRange) {
-        let hours = Math.floor(slideTimeRange / 60);
-        let minutes = slideTimeRange % 60;
-        return hours + ":" + Math.floor(minutes)
-    }
-
-    
-    for (let i = 1; i <= firstPartSlideAmount; i++) {
-    console.log(`Slide ${i} needs to be presented by ${slideTimeRangetoHours(slideTimeRange)}`)
-    slideTimeRange = slideTimeRange + timePerSlide
-    }
-}
-
-firstPartApresentation(startTime)
-/*
-
-
-
-
-/*
-const talkTotal = eventTotal - breakTotal
-
-// Time per slide
-const timePerSlide = talkTotal / slideNum
-
-// Break Time
-const breakStart = (eventTotal - breakTotal) / 2
-const breakEnd = breakStart + breakTotal
-
-// First part
-const firstPartRange = breakStart -  
-*/
+};
